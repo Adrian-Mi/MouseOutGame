b00fefa68e229d5000c35795033edf96ccc8b0b0
Adrian-Mi <120776299+Adrian-Mi@users.noreply.github.com>
2022-12-17 14:30:16 +0330

    Add files via upload

---
+++ WindowsFormsAppGame/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsAppGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(-1, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 29);
+            this.label1.TabIndex = 0;
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(46, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 79);
+            this.label2.TabIndex = 0;
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(71, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 0;
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(138, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 85);
+            this.label4.TabIndex = 0;
+            this.label4.MouseLeave += new System.EventHandler(this.label4_MouseLeave);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(162, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 24);
+            this.label5.TabIndex = 0;
+            this.label5.MouseLeave += new System.EventHandler(this.label5_MouseLeave);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label6.Location = new System.Drawing.Point(227, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 45);
+            this.label6.TabIndex = 0;
+            this.label6.MouseLeave += new System.EventHandler(this.label6_MouseLeave);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label7.Location = new System.Drawing.Point(194, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 45);
+            this.label7.TabIndex = 0;
+            this.label7.MouseEnter += new System.EventHandler(this.label7_MouseEnter);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(-1, 74);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Start";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ WindowsFormsAppGame/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGame
+{
+    public partial class Form1 : Form
+    {
+        bool flag ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            if (flag==false )
+                MessageBox.Show("you lose!!!");
+           
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            flag = true;
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            flag = false;
+            MessageBox.Show("you lose!");
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            if (flag == false)
+                MessageBox.Show("you lose!!!");
+          
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            if (flag == false)
+                MessageBox.Show("you lose!!!");
+          
+        }
+
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            if (flag == false)
+                MessageBox.Show("you lose!!!");
+           
+        }
+
+        private void label5_MouseLeave(object sender, EventArgs e)
+        {
+            if (flag == false)
+                MessageBox.Show("you lose!!!");
+           
+        }
+
+        private void label6_MouseLeave(object sender, EventArgs e)
+        {
+            if (flag == false)
+                MessageBox.Show("you lose!!!");
+            
+        }
+
+        private void label7_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("you win!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            flag = true;
+        }
+    }
+}
